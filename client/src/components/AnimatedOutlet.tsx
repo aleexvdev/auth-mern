16946eda6c608b70b44d4a9b15c03374d999ab88
aleexvdev <valverdealex25@gmail.com>
2024-04-21 15:09:12 -0500

    feat (config react router)

---
+++ client/src/components/AnimatedOutlet.tsx
@@ -0,0 +1,42 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { Outlet } from 'react-router-dom';
+
+export const AnimatedOutlet = () => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.3 }}
+        style={{
+          position: 'relative',
+          zIndex: 1,
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <motion.div
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          exit={{ scale: 0.8, opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          style={{
+            maxWidth: '800px',
+            width: '100%',
+            padding: '2rem',
+            backgroundColor: '#fff',
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+            borderRadius: '8px',
+          }}
+        >
+          <Outlet />
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { App } from './App.tsx'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import './index.css'
+import { ErrorPage, Root } from './routes'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const routerConfig = [
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        path: '/',
+        element: <App />
+      },
+    ]
+  }
+];
+
+const router = createBrowserRouter(routerConfig);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
