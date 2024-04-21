5da9be1a39185814a43faae5f2fa937f57e0cc2c
aleexvdev <valverdealex25@gmail.com>
2024-04-21 15:20:15 -0500

    fix (tailwind fix postcss)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import './index.css'
 import { ErrorPage, Root } from './routes'
+import './assets/index.css'
 
 const routerConfig = [
   {
