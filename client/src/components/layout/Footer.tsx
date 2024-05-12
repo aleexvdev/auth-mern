b1ee3ccad894ba301726207511f669378bad0891
aleexvdev <valverdealex25@gmail.com>
2024-05-12 12:24:41 -0500

    FEAT: link to socials

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -10,22 +10,22 @@ export const Footer = () => {
         <span className='text-white'>Developed by: Alexander Valverde</span>
       </div>
       <div className="flex items-center justify-end w-full gap-5">
-        <Link to={''}>
+        <Link to={'https://www.facebook.com/alexvalverde666/'}>
           <button className="text-white hover:text-blue-500">
             <RiFacebookFill className="w-6 h-6" />
           </button>
         </Link>
-        <Link to={''}>
+        <Link to={'https://www.instagram.com/alexvalverde._/'}>
           <button className="text-white hover:text-fuchsia-500">
             <FiInstagram className="w-6 h-6" />
           </button>
         </Link>
-        <Link to={''}>
+        <Link to={'https://www.linkedin.com/in/alexvdev/'}>
           <button className="text-white hover:text-blue-500">
             <GrLinkedinOption className="w-6 h-6" />
           </button>
         </Link>
-        <Link to={''}>
+        <Link to={'https://github.com/aleexvdev/'}>
           <button className="text-white hover:text-gray-500">
             <FiGithub className="w-6 h-6" />
           </button>
